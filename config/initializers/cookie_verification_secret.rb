96684372922c39fcf04e5055f466050d3fe61022
BenHall <Ben@Benhall.me.uk>
2010-06-04 00:35:43 +0100

    Removing rails 2.3.8 as Heroku wasn't happy

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '057facd99307f9f66657f3a8af0dec2a96cf7925801e055ec7193d47c489ecba232db47ad088062198ef9c6615f32192f4a99edb2422c3dfe1efea8f50aa1119';
